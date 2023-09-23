47ba4012a059a46be26da138dcd6d5b0ef075ca8
Lichtsteiner Mathieu mlichtst <65190189+Mathieu-Lichtsteiner@users.noreply.github.com>
2023-09-23 21:09:24 +0200

    basic implementation of calendar

--- libs/frontend/feature/calendar/src/lib/fe-calendar.component.ts
+++ libs/frontend/feature/calendar/src/lib/fe-calendar.component.ts
@@ -1,18 +1,57 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { map, of } from 'rxjs';
 
 @Component({
-  selector: 'life-fe-calendar',
   standalone: true,
   imports: [CommonModule],
-  template: `<p>fe-calendar works!</p>`,
+  template: `
+    <ul
+      *ngIf="this.points$ | async as points"
+      class="grid grid-flow-row grid-cols-[repeat(12,auto)] gap-3 w-min mx-auto">
+      <li
+        *ngFor="let point of points"
+        class="rounded-full aspect-square h-3"
+        [ngClass]="point.isPast ? 'bg-gray-600' : 'bg-gray-300'"></li>
+    </ul>
+  `,
   styles: [
     `
       :host {
-        @apply block;
+        @apply block container mx-auto px-3;
       }
     `
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FrontendFeatureCalendarComponent {}
+export class FrontendFeatureCalendarComponent {
+  private readonly _startDate$ = of(new Date(2000, 5, 7));
+
+  protected readonly points$ = this._startDate$.pipe(
+    map((startDate) => {
+      const todaysIndex = this.durationInDays(startDate, new Date());
+      const endDate = new Date(
+        startDate.getFullYear() + 100,
+        startDate.getMonth(),
+        startDate.getDay()
+      );
+      const points = new Array(this.durationInDays(startDate, endDate));
+      points.fill({ isPast: true }, 0, todaysIndex);
+      points.fill({ isPast: false }, todaysIndex, points.length);
+      return points;
+    })
+  );
+
+  private durationInDays(start: Date, end: Date) {
+    return this.getIntervallsFromMs(end.getTime() - start.getTime());
+  }
+
+  private getIntervallsFromMs(ms: number): number {
+    const seconds = Math.floor(ms / 1_000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const months = Math.floor(days / 30);
+    return months;
+  }
+}
