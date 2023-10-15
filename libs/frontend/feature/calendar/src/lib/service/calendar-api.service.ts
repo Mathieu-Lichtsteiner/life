55d27cc906e44957d2fe572982bfd337ccc1c92a
Lichtsteiner Mathieu mlichtst <65190189+Mathieu-Lichtsteiner@users.noreply.github.com>
2023-10-15 23:24:36 +0200

    inject apiEndpointService

--- libs/frontend/feature/calendar/src/lib/service/calendar-api.service.ts
+++ libs/frontend/feature/calendar/src/lib/service/calendar-api.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { ApiEndpointService } from '@life/fe/util/api';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CalendarApiService {}
+export class CalendarApiService {
+  private readonly apiEndpointService = inject(ApiEndpointService);
+}
