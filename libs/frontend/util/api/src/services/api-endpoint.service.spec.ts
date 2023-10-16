b7a8cc4013d98726c25ff75fbfca5ca2d87fe4b0
Lichtsteiner Mathieu mlichtst <65190189+Mathieu-Lichtsteiner@users.noreply.github.com>
2023-10-16 03:15:15 +0200

    base tests

--- libs/frontend/util/api/src/services/api-endpoint.service.spec.ts
+++ libs/frontend/util/api/src/services/api-endpoint.service.spec.ts
@@ -1,12 +1,22 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ApiEndpointService } from './api-endpoint.service';
+import { BASE_URL } from '../tokens/base-url';
 
 describe(ApiEndpointService.name, () => {
   let service: ApiEndpointService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: BASE_URL,
+          useValue: './'
+        }
+      ]
+    });
     service = TestBed.inject(ApiEndpointService);
   });
 
